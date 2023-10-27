e0d6f54ea1ae99dec1c407a388b0aa1ac130bacc
Nartey Benjamin <benjamin.narteyteye@kibo.school>
2023-10-27 13:36:32 +0000

    fizz

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 
 app=Flask(__name__)
+
 @app.route("/")
 def fizzbuzz():
     return render_template('fizzbuzz.html' )
+
 if __name__=="__main__":
     app.run(debug=True)
